e7f02eb83a5952060fdea5fbb250e56f9606441a
andrew <andrewjthomsen@yahoo.com>
2019-12-29 14:37:12 -0800

    Made changes to index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 
 
-const index = () => <div>Welcome Page!</div>
+// const index = () => <div>Welcome Page!</div>
+import React from 'react';
+class Index extends React.Component {
+    render () {
+        return (
+            <h1>I am Index from Class Component</h1>
+        )
+    }
+}
 
-export default index;
+export default Index;
