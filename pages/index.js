e08b1c60fb2175e2dfc6c02fd24c49a1e3a53145
andrew <andrewjthomsen@yahoo.com>
2020-01-06 19:18:54 -0800

    Restructured index.js page and deleted unused code

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,9 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
-import SuperComponent from "../components/SuperComponent";
-import axios from "axios";
-
-class Index extends SuperComponent {
-  static async getInitialProps() {
-    let userData = {};
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      const userData = response.data;
-      console.log(response);
-    } catch (err) {
-      console.error(err);
-    }
-
-    return { initialData: [1, 2, 3, 4] };
-  }
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      title: "I am Index Pge"
-    };
-  }
-  componentDidMount() {
-    console.log("componentDidMount says... I am also working");
-  }
-
-  componentDidUpdate() {
-    console.log("componentDidUpdate is working here!");
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
-  updateTitle = () => {
-    this.setState({ title: "Pow!" });
-  };
+import { Button } from "reactstrap";
+class Index extends React.Component {
   render() {
-    const { title } = this.state;
-    const { userData, initialData } = this.props;
-
-    return (
-      <BaseLayout>
-        <h1>I am Index Page from Class Component</h1>
-        <h2>{title}</h2>
-        <button onClick={() => this.updateTitle()}>Change Title</button>
-      </BaseLayout>
-    );
+    return <BaseLayout></BaseLayout>;
   }
 }
 
